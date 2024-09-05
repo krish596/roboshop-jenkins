659ba6340671ee09418321010b8544693f917c36
Chimtamreddy <kr7348202@gmail.com>
2024-09-05 22:00:22 +0530

    jenkins

--- shared-library/vars/cipipeline.groovy
+++ shared-library/vars/cipipeline.groovy
@@ -1,62 +1,26 @@
-
 def call() {
-    pipeline {
-        agent any
+    node('workstation') {
+        stage('Compile Code') {
+            common.compile()
 
-        stages {
-            stage('Compile Code') {
+        }
 
-                steps {
-                    sh 'env'
-                }
-            }
+        stage('Test') {
+            print 'Hello'
+        }
 
-            stage('Test') {
-                when {
-                    allOf {
-                        expression { env.BRANCH_NAME != null }
-                        expression { env.TAG_NAME == null }
-                    }
-                }
-                steps {
-                    echo 'Hello World'
-                }
-            }
+        stage('Code Quality') {
+            print 'Hello'
+        }
 
-            stage('Code Quality') {
+        stage('Code Security') {
+            print 'Hello'
+        }
 
-                when {
-                    allOf {
-                        expression { env.BRANCH_NAME != null }
-                        expression { env.TAG_NAME == null }
-                    }
-                }
-
-
-                steps {
-                    echo 'Hello World'
-
-                }
-            }
-
-            stage('Code Security') {
-                when {
-                    expression { BRANCH_NAME == "main" }
-                }
-                steps {
-                    echo 'Hello World'
-                }
-            }
-
-            stage('Release') {
-                when {
-                    expression { env.TAG_NAME ==~ ".*" }
-                }
-                steps {
-                    sh 'env'
-                    echo 'Hello World'
-                }
-            }
+        stage('Release') {
+            print 'Hello'
         }
     }
 }
+
+
---
+++ shared-library/vars/common.groovy
@@ -0,0 +1,18 @@
+def complie() {
+    if (env.codeType == "maven") {
+        print 'Maven'
+    }
+
+    if (env.codeType == "nodejs") {
+        print 'NodeJs'
+    }
+
+    if (env.codeType == "python") {
+        print 'Python'
+    }
+
+    if (env.codeType == "static") {
+        print 'Static'
+    }
+
+}
