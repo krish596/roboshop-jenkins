bbbd0246c27644c98a411d18641cabdb64483597
Chimtamreddy <kr7348202@gmail.com>
2024-09-06 20:37:27 +0530

    jenkins

--- shared-library/vars/cipipeline.groovy
+++ shared-library/vars/cipipeline.groovy
@@ -2,7 +2,7 @@ def call() {
     node('workstation') {
         sh "find . | sed -e '1d' |xargs rm -rf "
         sh 'env'
-        git branch: 'main', url: "https://github.com/krish596/${component}"
+        git branch: "${ BRANCH_NAME }", url: "https://github.com/krish596/${component}"
 
 
         stage('Compile Code') {
