c325ae544547b537b330920c7693f939b655f433
Chimtamreddy <kr7348202@gmail.com>
2024-09-07 14:34:57 +0530

    jenkins

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -19,7 +19,17 @@ def compile() {
 
 def test() {
     stage('Test Cases') {
-        print 'Test'
+        if (env.codeType == "maven") {
+            sh '/home/centos/maven/bin/mvn test'
+        }
+
+        if (env.codeType == "nodejs") {
+            sh 'npm test'
+        }
+
+        if (env.codeType == "python") {
+            sh 'python -m unittest'
+        }
     }
 }
 
