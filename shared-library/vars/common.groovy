2079aabcabd12a1206ac50c61b545f621096eb41
Chimtamreddy <kr7348202@gmail.com>
2024-09-09 11:55:30 +0530

    jenkins

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -69,7 +69,7 @@ def release() {
     stage('Release') {
         env.nexususer = sh (script: 'aws ssm get-parameter --name "nexus.username" --with-decryption --query "Parameter.Value" |xargs', returnStdout: true).trim()
 
-        env.nexuspass = sh (script: 'aws ssm get-parameter --name "nexus.pass" --with-decryption --query "Parameter.Value" |xargs', returnStdout: true).trim()
+        env.nexuspass = sh (script: 'aws ssm get-parameter --name "nexus.password" --with-decryption --query "Parameter.Value" |xargs', returnStdout: true).trim()
         wrap([$class: "MaskPasswordsBuildWrapper", varPasswordPairs: [[password: nexuspass]]]){
 
             if(env.codeType == "maven") {
